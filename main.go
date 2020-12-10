a273df6ef5361de610a2a4b02c0633e7d5d9df77
Bjørn Fjukstad <bfj@dips.no>
2020-12-10 23:49:14 +0100

    pretty and logging and stuff

--- main.go
+++ main.go
@@ -1,53 +1,48 @@
 package main
 
 import "fmt"
-
+import "time"
 import "html/template"
 import "net/http"
 
 import "github.com/fjukstad/headwind/yr"
 
 func main() {
-	fc, err := yr.GetForecast(0, 0)
-	if err != nil {
-		fmt.Println("Could not get forecast", err)
-	}
-	// bearing = 142.525
-
-	direction := fc.Properties.TimeSeries[0].Data.Instant.Details.WindFromDirection
-	speed := fc.Properties.TimeSeries[0].Data.Instant.Details.WindSpeed
-
-	bearing := 142.5
-
-	// to work
-	headwind := false
-	if direction < 90 && direction > 200 {
-		headwind = true
-	}
-
-	tailwind := !headwind
-
-	fmt.Println(direction - bearing)
-
-	fmt.Println("Headwind", headwind)
-
-	fmt.Println("tail", tailwind)
-
-	description := ""
-	if speed < 4 {
-		description = "lett"
-	} else if speed < 8 {
-		description = "tung"
-	} else {
-		description = "kjør bil"
-	}
-
-	fmt.Println(description)
 	tmpl := template.Must(template.ParseFiles("index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fc, err := yr.GetForecast(0, 0)
+		if err != nil {
+			fmt.Println("Could not get forecast", err)
+		}
+		// bearing = 142.525
+		t := time.Now()
+		fmtTime := t.Format("2006-01-02 15:04:05")
+		fmt.Printf("%s %s %s %s\n", fmtTime, r.RemoteAddr, r.Method, r.URL)
+
+		direction := fc.Properties.TimeSeries[0].Data.Instant.Details.WindFromDirection
+		speed := fc.Properties.TimeSeries[0].Data.Instant.Details.WindSpeed
+
+		//		bearing := 142.5
+
+		// to work
+		headwind := false
+		if direction < 90 && direction > 200 {
+			headwind = true
+		}
+
+		description := ""
+		if speed < 4 {
+			description = "lett motvind"
+		} else if speed < 8 {
+			description = "tung motvind"
+		} else {
+			description = "kjør bil"
+		}
+
 		data := WindInfo{
 			Headwind:    headwind,
 			Description: description,
+			Speed:       speed,
 		}
 		tmpl.Execute(w, data)
 	})
@@ -57,4 +52,5 @@ func main() {
 type WindInfo struct {
 	Headwind    bool
 	Description string
+	Speed       float64
 }
